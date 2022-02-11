758fed6c2d5e7c563b22c87a0bfb407a703d9697
DenizDumanli <denqza1999@gmail.com>
2022-02-11 15:33:21 +0300

    Liqubase added.

---
+++ src/main/resources/db/changelog/1_create_demo_table.sql
@@ -0,0 +1,10 @@
+-- public.course definition
+
+-- Drop table
+
+-- DROP TABLE public.course;
+
+CREATE TABLE public.course (
+                               id bigserial NOT NULL,
+                               course_name varchar NOT NULL
+);
